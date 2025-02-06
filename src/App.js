7c26a5a1d2acd64ea9fdf65ab839ff02e1d988d4
thegabrielmfreitas <gabrielmfreitas@gmail.com>
2025-02-06 14:54:38 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101497594</h1>
+      <h1>Student Name: Gabriel Mello de Freitas</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Gabriel Freitas
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
